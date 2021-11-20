af158512d324fea26f066226f61d857a14869b6f
τєαм ϰмαяτγ <85209355+S780821@users.noreply.github.com>
2021-11-21 00:44:43 +0530

    Update userbot.py

--- handlers/userbot.py
+++ handlers/userbot.py
@@ -10,36 +10,36 @@ from callsmusic.callsmusic import client as XMARTYUB
 from handlers.ownerstuff import _check_heroku
 
 # To Block a PM'ed User
-@ZAIDUB.on_message(filters.private & filters.command("block", [".", "/"]) & filters.me & ~filters.edited)
+@XMARTYUB.on_message(filters.private & filters.command("block", [".", "/"]) & filters.me & ~filters.edited)
 async def ubblock(_, message: Message):
   shit_id = message.chat.id
   gonna_block_u = await message.edit_text("`Blocking User...`")
   try:
-    await ZAIDUB.block_user(shit_id)
+    await XMARTYUB.block_user(shit_id)
     await gonna_block_u.edit("`Successfully Blocked This User`")
   except Exception as lol:
     await gonna_block_u.edit(f"`Can't Block This Guy! May be this is durov?` \n\n**Error:** `{lol}`")
 
 
 # To Unblock User That Already Blocked
-@ZAIDUB.on_message(filters.command("unblock", [".", "/"]) & filters.me & ~filters.edited)
+@XMARTYUB.on_message(filters.command("unblock", [".", "/"]) & filters.me & ~filters.edited)
 async def ubblock(_, message: Message):
   good_bro = int(message.command[1])
   gonna_unblock_u = await message.edit_text("`Unblocking User...`")
   try:
-    await ZAIDUB.unblock_user(good_bro)
+    await XMARTYUB.unblock_user(good_bro)
     await gonna_unblock_u.edit(f"`Successfully Unblocked The User` \n**User ID:** `{good_bro}`")
   except Exception as lol:
     await gonna_unblock_u.edit(f"`Can't Unblock That Guy!, I think he is still dumb!` \n\n**Error:** `{lol}`")
 
 
 # To Get How Many Chats that you are in (PM's also counted)
-@ZAIDUB.on_message(filters.private & filters.command("chats", [".", "/"]) & filters.me & ~filters.edited)
+@XMARTYUB.on_message(filters.private & filters.command("chats", [".", "/"]) & filters.me & ~filters.edited)
 async def ubgetchats(_, message: Message):
   getting_chats = await message.edit_text("`Checking Your Chats, Hang On...`")
-  async for dialog in ZAIDUB.iter_dialogs():
+  async for dialog in XMARTYUB.iter_dialogs():
     try:
-      total = await ZAIDUB.get_dialogs_count()
+      total = await XMARTYUB.get_dialogs_count()
       await getting_chats.edit(f"**Total Dialogs Counted:** `{total}` \n\n**Not Stable Lol**")
     except Exception as lol:
       brokenmsg = await message.reply_text(f"`Never Gonna Give You Up!, but Something Went Wrong!`")
@@ -47,7 +47,7 @@ async def ubgetchats(_, message: Message):
 
 
 # Leave From a Chat
-@ZAIDUB.on_message(filters.command("kickme", [".", "/"]) & filters.me & ~filters.edited)
+@XMARTYUB.on_message(filters.command("kickme", [".", "/"]) & filters.me & ~filters.edited)
 async def ubkickme(_, message: Message):
   i_go_away = await message.edit_text("`Leaving This Chat...`")
   try:
@@ -58,11 +58,11 @@ async def ubkickme(_, message: Message):
 
 
 # Alive Message
-@ZAIDUB.on_message(filters.command("alive", [".", "/"]) & filters.me & ~filters.edited)
+@XMARTYUB.on_message(filters.command("alive", [".", "/"]) & filters.me & ~filters.edited)
 async def ubalive(_, message: Message):
   alive_msg = await message.edit_text("`Processing...`")
   alive_pic = "https://telegra.ph/file/8848147cd4fb6af2f094f.jpg"
-  await message.reply_photo(alive_pic, caption=f"**☑️ Zaid Music Userbot is Alive 🌀** \n\n**🤖 Version** \n ↳**Bot Version:** `{z_version}` \n ↳**Userbot Version:** `{zaidub_version}` \n\n**🐬 Info**\n ↳**Music Bot:** @{BOT_USERNAME} \n ↳**Owner:** [Click Here](tg://user?id={BOT_OWNER})")
+  await message.reply_photo(alive_pic, caption=f"**☑️ Zaid Music Userbot is Alive 🌀** \n\n**🤖 Version** \n ↳**Bot Version:** `{z_version}` \n ↳**Userbot Version:** `{xmartyub_version}` \n\n**🐬 Info**\n ↳**Music Bot:** @{BOT_USERNAME} \n ↳**Owner:** [Click Here](tg://user?id={BOT_OWNER})")
   await alive_msg.delete()
 
 
@@ -70,7 +70,7 @@ async def ubalive(_, message: Message):
 PM_LOGS = os.environ.get("PM_LOGS", "")
 PM_LOG_CHAT_ID = int(os.environ.get("PM_LOG_CHAT_ID", 12345678))
 
-@ZAIDUB.on_message(filters.private & filters.command("pmlogs", [".", "/"]) & filters.me & ~filters.edited)
+@XMARTYUB.on_message(filters.private & filters.command("pmlogs", [".", "/"]) & filters.me & ~filters.edited)
 @_check_heroku
 async def getlogs(client: XMARTYUB, message: Message, app_):
   if len(message.command) != 2:
@@ -91,7 +91,7 @@ async def getlogs(client: XMARTYUB, message: Message, app_):
       chat_id = pmchat.id
       await XMARTYUB.set_chat_photo(chat_id=chat_id, photo=chat_pic)
       await logmsg.edit(f"`Successfully Finished Step 1, To Enable This Feature Please Check Your Log Group That Created Now!!` \n\n ~ **@XMARTY_SUPPORT**")
-      await client.send_message(chat_id, f"**Welcome to @{(await XMARTYUB.get_me()).username}'s PM Log Group!** \nThis Chat will Contain All PM Messages Of **@{(await ZAIDUB.get_me()).username}** ! \n\n\n`/setvar PM_LOG_CHAT_ID {chat_id}` \n\n ✪ **Please Copy and Send Above Command To Your @{BOT_USERNAME} Now**!")
+      await client.send_message(chat_id, f"**Welcome to @{(await XMARTYUB.get_me()).username}'s PM Log Group!** \nThis Chat will Contain All PM Messages Of **@{(await XMARTYUB.get_me()).username}** ! \n\n\n`/setvar PM_LOG_CHAT_ID {chat_id}` \n\n ✪ **Please Copy and Send Above Command To Your @{BOT_USERNAME} Now**!")
     except Exception as lol:
       await logmsg.edit(f"`Can't Enable This Feature!, Something Wrong Happend!` \n\n**Error:** `{lol}`")
       return
@@ -109,13 +109,13 @@ async def getlogs(client: XMARTYUB, message: Message, app_):
       await message.edit_text(f"`Can't Remove This Feature! Maybe You Didn't Enabled It?` \n\n**Error:** {lol}")
 
 
-@ZAIDUB.on_message(filters.private)
-async def sendpmlol(client: ZAIDUB, message: Message):
-  ZAIDUB_ID = int((await ZAIDUB.get_me()).id)
-  if message.from_user.id == BOT_OWNER or message.from_user.id == ZAIDUB_ID:
+@XMARTYUB.on_message(filters.private)
+async def sendpmlol(client: XMARTYUB, message: Message):
+  XMARTYUB_ID = int((await XMARTYUB.get_me()).id)
+  if message.from_user.id == BOT_OWNER or message.from_user.id == XMARTYUB_ID:
     return
   pmlogchat = PM_LOG_CHAT_ID
-  userinfo = await ZAIDUB.get_users(user_ids=message.from_user.id)
+  userinfo = await XMARTYUB.get_users(user_ids=message.from_user.id)
   nibba = int(message.chat.id)
   msg_txt = message.text
   if PM_LOG_CHAT_ID == 12345678:
