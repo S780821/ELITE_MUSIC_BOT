f0a3f6f569beb0cdbe530c3578a2ceeb10665dcd
τєαм ϰмαяτγ <85209355+S780821@users.noreply.github.com>
2021-11-21 08:40:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -23,8 +23,8 @@ BOT_USERNAME = os.environ.get("BOT_USERNAME") # Your Bot's Username without "@"
 DATABASE_URL = os.environ.get("DATABASE_URL") #mongo database url for more info contact in support group
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL")) # Your Log Channel! Make a private channel and get it's ID
 BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False)) # If you need to broadcast messages as a copy or Forwarded Message
-THUMB_URL = os.environ.get("THUMB_URL", "https://telegra.ph/file/c16cbc1b51444436477c6.jpg")
-XMARTY_QUE = os.environ.get("XMARTY_QUE", "https://telegra.ph/file/c16cbc1b51444436477c6.jpg")
+THUMB_URL = os.environ.get("THUMB_URL", "https://telegra.ph/file/e10da38412ca7fe5bce30.jpg")
+XMARTY_QUE = os.environ.get("XMARTY_QUE", "https://telegra.ph/file/0ae55070cf0c4413817f8.jpg")
 UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "XMARTY_UPDATES")
 XMARTY_SUPPORT = os.environ.get("XMARTY_SUPPORT", "XMARTY_SUPPORT")
 
